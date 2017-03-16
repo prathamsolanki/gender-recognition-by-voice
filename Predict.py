e557ff9570e2a5b7f92f10178562e2e015386765
Pratham Solanki <pratham.c.solanki@gmail.com>
2017-03-16 20:43:53 +0530

    app initial commit

---
+++ Predict.py
@@ -0,0 +1,23 @@
+import subprocess
+import pandas as pd
+import xgboost as xgb
+
+subprocess.call ("/usr/bin/Rscript --vanilla /home/prathamsolanki/github/gender-recognition-by-voice/ExtractFeatures.R", shell=True)
+
+features_to_use = ["meanfreq","sd","median","Q25","Q75","IQR","skew","kurt","sp.ent","sfm","mode","centroid","meanfun","minfun","maxfun","meandom","mindom","maxdom","dfrange","modindx"]
+
+test_df = pd.read_csv('Data/Amy-Acoustics.csv')
+
+test_X = test_df[features_to_use]
+xgtest = xgb.DMatrix(test_X)
+
+model = xgb.Booster({'nthread':4})
+model.load_model("voice-gender.model")
+
+pred_test_y = model.predict(xgtest)
+
+if pred_test_y >= 0.5:
+    print "male"
+
+else:
+    print "female"
---
+++ Train.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from scipy import sparse
+import xgboost as xgb
+from sklearn import model_selection, preprocessing, ensemble
+from sklearn.metrics import log_loss
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.cross_validation import train_test_split
+
+def runXGB(X, y):
+    param = {}
+    param['objective'] = 'binary:logistic'
+    param['eta'] = 0.01                     #default: 0.3
+    param['max_depth'] = 6                  #default: 6
+    param['silent'] = 1
+    param['gamma'] = 0                      #default: 0
+    param['eval_metric'] = "error"
+    param['min_child_weight'] = 4           #default: 1
+    param['max_delta_step'] = 4             #default: 0
+    param['subsample'] = 0.7                  #default: 1
+    param['colsample_bytree'] = 0.7           #default: 1
+    param['seed'] = 0
+    num_rounds = 100
+
+    plst = list(param.items())
+
+    data_train, data_cv, labels_train, labels_cv = train_test_split(X, y, test_size=0.20, random_state=42)
+
+    xgtrain = xgb.DMatrix(data_train, label = labels_train)
+    xgcv = xgb.DMatrix(data_cv, label = labels_cv)
+
+    evallist = [(xgcv,'eval')]
+    model = xgb.train(plst, xgtrain, num_rounds, evallist, early_stopping_rounds = 10)
+
+    model.save_model('voice-gender.model')
+
+train_df = pd.read_csv('Data/voice.csv')
+
+X = train_df.ix[:, 0:20]
+y = train_df["label"]
+y = y.replace({'male':1,'female':0})
+
+runXGB(X, y)
